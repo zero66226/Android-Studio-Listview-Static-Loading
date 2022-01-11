d957e1065059245c2da1d6c8add7d326787b1eb2
Chen66226 <84027226+zero66226@users.noreply.github.com>
2022-01-11 23:26:34 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/listviewstaticloading/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.listviewstaticloading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.tv_select);
+        ListView listView = findViewById(R.id.mylist);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                tv.setText("你選擇了:"+((TextView) view).getText());
+            }
+        });
+
+    }
+}
